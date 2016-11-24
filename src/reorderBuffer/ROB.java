f38064793584b644385fb01e08e169e2d78e53b8
HatemMorgan <hatemmorgan17@gmail.com>
2016-11-24 04:57:42 +0200

    finishing ROB and testing every single case

--- src/reorderBuffer/ROB.java
+++ src/reorderBuffer/ROB.java
@@ -4,6 +4,7 @@ public class ROB {
 	private ROBEntry head;
 	private ROBEntry tail;
 	private ROBEntry first;
+	private ROBEntry last;
 	private int size ;
 
 	
@@ -28,13 +29,12 @@ public class ROB {
 			if(i== size){
 				// last entry in the ROB linkedlist data structure
 				ROBEntry r = new ROBEntry(i,null,null, null, false);
-				
+				last = r;
 				// linking the before last with last one
-				current.next = r;
+				current.next = last;
 				
 				// link the last entry with the first one to act as a circular linked list
-				current = current.next;
-				current.next = first;
+				last.next = first;
 			
 				continue;
 			}
@@ -50,9 +50,13 @@ public class ROB {
 	
 	
 	private void insertToROB(ROBEntryType type, ROBEntryDestination destination){
+		if(!isFull()){
 		tail.setType(type);
 		tail.setDestination(destination);
 		tail= tail.next;
+		}else{
+			System.out.println("ROB is Full you cannot insert a new Entry");
+		}
 	}
 	
 	private void writeResultTOROB(int entryNumber,String result){
@@ -81,8 +85,53 @@ public class ROB {
 	}
 	
 	
-	// TODO commiting and testing
-	
+	private void commit(int entryNumber){
+		// check if the head is pointing to this entry inorder to maintain inorder committing 
+		if(head.getEntryNumber() == entryNumber && head.isReady()){
+			ROBEntry current = head.next;
+			
+			if(head.equals(first)){
+				head = new ROBEntry(head.getEntryNumber(),null,null,null,false);
+				head.next = current ;
+				first = head;		// emptying the first entry
+				head = current;    // moving head to the next entry
+				last.next = first; // connecting last to new first to maintain that the linked list is circular
+				tail = tail.next; // moving tail to next empty element
+				return;
+
+			}
+				ROBEntry prevHead = first ;
+				while(!prevHead.next.equals(head)){
+					prevHead = prevHead.next;
+				}
+			
+				head = new ROBEntry(head.getEntryNumber(),null,null,null,false);
+				head.next = current ;
+				prevHead.next = head ; // connecting the previous entry with the new empty entry
+				head = current; // moving head to next entry
+				tail = tail.next;
+			
+			
+		
+			
+			System.out.println("entry with number #"+entryNumber+" is committed and removed from the ROB");
+		}else{
+			if (head.getEntryNumber() != entryNumber) {
+			System.out.println("Cannot commit because the head is not pointing to this entry = #"+entryNumber+" .");
+			return;
+			}
+			
+			if(!head.isReady()){
+				System.out.println("Connot commit because the ROB entry the head is pointing to it is not ready(instruction is not in commit stage) ");
+			}
+			
+		}
+	}
+	// check if the head and tail are pointing to the same entry
+	// and also check that head.next has type != null to make sure that we are not at the begining
+	private boolean isFull(){
+		return head.equals(tail)&&head.next.getType()!=null ;
+	}
 	
 	private void printROB(){
 		ROBEntry current = first ;
@@ -92,11 +141,35 @@ public class ROB {
 		}
 		// print the last link in the 
 		System.out.println(current.toString());
+		
 	}
 	
 	
 	public static void main(String[] args) {
-		ROB r = new ROB(10);
+		ROB r = new ROB(4);
+		r.insertToROB(ROBEntryType.LD, ROBEntryDestination.R1);
+		r.insertToROB(ROBEntryType.SD, ROBEntryDestination.R2);
+		r.insertToROB(ROBEntryType.LD, ROBEntryDestination.R3);
+		r.insertToROB(ROBEntryType.INT, ROBEntryDestination.R4);
+		r.insertToROB(ROBEntryType.INT, ROBEntryDestination.R5); // testing to insert a new entry when the ROB is FUll
+		
+		r.writeResultTOROB(3, "24"); // testing entring a new entry
+		r.commit(3); // testing committing an entry that the head is not pointing to it
+		r.commit(1); // testing committing an entry that is not ready
+		
+		r.writeResultTOROB(1,"200");
+		r.commit(1); // testing to commit an entry that is ready
+		
+		// testing committing all ROB entries
+		r.writeResultTOROB(2, "1000");
+		r.writeResultTOROB(4, "500");
+		r.commit(2);
+		r.commit(3);
+		r.commit(4);
+		
+		r.insertToROB(ROBEntryType.INT, ROBEntryDestination.R6); // testing if the new entry will be in the first ROB entry
+		
+		System.out.println("------------------------------------------");
 		r.printROB();
 	}
 	
